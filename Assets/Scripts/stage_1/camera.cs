5bd97ab0d949b6130d2f06c80f21bf91af8f764d
mpataki <matpataki@gmail.com>
2015-02-08 00:12:34 -0500

    adds camera player tracking

---
+++ Assets/Scripts/stage_1/camera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	Vector3 offset;
+	Player player;
+
+	// Use this for initialization
+	void Start () {
+		GameObject profile_go = GameObject.Find ("Player");
+		player = (Player)profile_go.GetComponent (typeof(Player));
+		offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var cameraScreenCenter = (Camera.main.camera.WorldToScreenPoint( Camera.main.camera.transform.position ));
+		var yOffset = Camera.main.camera.orthographicSize * 0.4f;
+		var xOffset = yOffset * Camera.main.camera.aspect;
+		var leftEdge  = cameraScreenCenter.x - xOffset;
+		var rightEdge = cameraScreenCenter.x + xOffset;
+		var playerScreenPos = camera.WorldToScreenPoint (player.transform.position);
+
+		if (playerScreenPos.x <= leftEdge)
+			this.transform.Translate( - Mathf.Abs( leftEdge - playerScreenPos.x), 0, 0 );
+		else if (playerScreenPos.x >= rightEdge)
+			this.transform.Translate( Mathf.Abs( rightEdge - playerScreenPos.x ), 0, 0 );
+
+	}
+}
