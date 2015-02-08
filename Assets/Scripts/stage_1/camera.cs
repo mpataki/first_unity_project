6400f32103aa8d3b29954868dabe0a573c7cffd9
mpataki <matpataki@gmail.com>
2015-02-08 11:29:28 -0500

    bounds camera edge to bg

--- Assets/Scripts/stage_1/camera.cs
+++ Assets/Scripts/stage_1/camera.cs
@@ -5,27 +5,39 @@ public class camera : MonoBehaviour {
 
 	Vector3 offset;
 	Player player;
+	Vector3 bgMinScreenPoint;
+	Vector3 bgMaxScreenPoint;
 
 	// Use this for initialization
 	void Start () {
-		GameObject profile_go = GameObject.Find ("Player");
-		player = (Player)profile_go.GetComponent (typeof(Player));
-		offset = this.transform.position - player.transform.position;
+		GameObject profile_go = GameObject.Find("Player");
+		player = (Player)profile_go.GetComponent(typeof(Player));
+		offset = transform.position - player.transform.position;
+
+		GameObject bg = GameObject.Find("Background");
+		bgMinScreenPoint = bg.renderer.bounds.min;
+		bgMaxScreenPoint = bg.renderer.bounds.max;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var cameraScreenCenter = (Camera.main.camera.WorldToScreenPoint( Camera.main.camera.transform.position ));
-		var yOffset = Camera.main.camera.orthographicSize * 0.4f;
-		var xOffset = yOffset * Camera.main.camera.aspect;
+		var cameraScreenCenter = (camera.WorldToScreenPoint( camera.transform.position ));
+		var yOffset = camera.orthographicSize * 0.3f;
+		var xOffset = yOffset * camera.aspect;
 		var leftEdge  = cameraScreenCenter.x - xOffset;
 		var rightEdge = cameraScreenCenter.x + xOffset;
 		var playerScreenPos = camera.WorldToScreenPoint (player.transform.position);
 
-		if (playerScreenPos.x <= leftEdge)
-			this.transform.Translate( - Mathf.Abs( leftEdge - playerScreenPos.x), 0, 0 );
-		else if (playerScreenPos.x >= rightEdge)
-			this.transform.Translate( Mathf.Abs( rightEdge - playerScreenPos.x ), 0, 0 );
+		Vector3 halfScreenWidth = new Vector3(camera.orthographicSize * camera.aspect, 0, 0);
+		var leftCameraEdge  = camera.transform.position - halfScreenWidth;
+		var rightCameraEdge = camera.transform.position + halfScreenWidth;
 
+		float leftDelta  = Mathf.Abs( leftEdge - playerScreenPos.x );
+		float rightDelta = Mathf.Abs( rightEdge - playerScreenPos.x );
+
+		if (playerScreenPos.x <= leftEdge && (leftCameraEdge.x - leftDelta) > bgMinScreenPoint.x)
+			transform.Translate( - leftDelta, 0, 0 );
+		else if (playerScreenPos.x >= rightEdge && (rightCameraEdge.x + rightDelta) < bgMaxScreenPoint.x )
+			transform.Translate( rightDelta, 0, 0 );
 	}
 }
